a86b7bfba6a9c5f6043a6b7f488c0d9034a37747
Alexandr Wokalek <wokalek@wokalek.ru>
2026-01-26 22:34:53 +0300

    fix: useOptions, added subscriber removal

--- src/composables/useOptions.ts
+++ src/composables/useOptions.ts
@@ -1,9 +1,11 @@
 export default function () {
   const optionsRef = ref(_.cloneDeep($options.value))
 
-  $options.subscribe((value) => {
+  const unsubscribe = $options.subscribe((value) => {
     optionsRef.value = _.cloneDeep(value)
   })
+
+  if (getCurrentScope()) onScopeDispose(unsubscribe)
 
   return optionsRef
 }
